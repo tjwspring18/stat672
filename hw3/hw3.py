c711a62f5cae975bbbfc4ef8bae08a896ef3a6ed
tom <tjwall000@gmail.com>
2018-03-08 21:57:30 -0500

    hw3.py

---
+++ hw3/hw3.py
@@ -0,0 +1,33 @@
+import sklearn as skl
+import numpy as np
+import pandas as pd
+
+# read data
+X = pd.read_csv('hw3_X.csv', sep=',', header = None)
+Y = pd.read_csv('hw3_Y.csv', sep=',', header = None)
+
+# function to generate \Phi from X
+def GeneratePhiMatrix(X):
+	print("foo")
+
+# function to create training matrix
+def GenerateTrainingMatrix(X):
+
+# function to create test matrix
+def GenerateTestMatrix(X):
+
+# function to center matrix
+def CenterMatrix(X):
+
+# function to scale matrix
+def ScaleMatrix(X):
+
+# generate lambda
+
+# compute SVD of X_train
+
+# calculate \hat{w}_{ridge}
+
+# function to compute test error
+
+
